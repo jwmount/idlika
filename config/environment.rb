5e137e91bc8c39e25ff7ed22a93ea88bfa32989c
jwmount <john@venuesoftware.com>
2010-05-07 09:26:09 -0700

    0.115 -- nav bar links working better.  If user not signed up or logged in takes them to these options.

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
-ENV['IDLIKA_VERSION'] = '0.014'
+ENV['IDLIKA_VERSION'] = '0.015'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
