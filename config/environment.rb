83709f23dcb6283fb5729eacf52cf7913cfe64bf
jwmount <john@venuesoftware.com>
2010-07-20 17:43:13 -0700

    uncomment RAILS_GEM_VERSION

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@
 # require 'action_mailer'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-# RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 ENV['IDLIKA_VERSION'] = '0.1.036'
 
 ENV['S3_BUCKET'] = "idlika.com"
