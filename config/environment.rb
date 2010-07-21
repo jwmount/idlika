a6e6c847d2d21c5a9fec88afc14a816fab50e3b3
jwmount <john@venuesoftware.com>
2010-07-20 17:39:22 -0700

    rmoved all references to action_mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # MUST be on for dev, MUST remove this for staging and production
- require 'action_mailer'
+# require 'action_mailer'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 # RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
@@ -66,5 +66,21 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_content_method = :smtp
+    config.action_mailer.smtp_settings = {
+  #  ActionMailer::Base.delivery_method = :smtp
+  #  ActionMailer::Base.default_content_type = "text/html"
+  #  ActionMailer::Base.smtp_settings = {
+      :address        => "smtp.sendgrid.net",
+      :port           => "25",
+      :authentication => :login,
+  #    :user_name      => ENV['IDLIKA_EMAIL_NAME'],
+      :user_name      => ENV['SENDGRID_USERNAME'],
+  #    :password       => ENV['IDLIKA_EMAIL_PASSWORD'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => ENV['IDLIKA_DOMAIN'],
+    }
   
 end
