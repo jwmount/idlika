7611646259b80a27a54a2314a87a15b5c694db74
jwmount <john@venuesoftware.com>
2010-08-27 13:59:01 -0700

    0.1.051.  Better version moved from -dev of same level.

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require 'net/http'
 # Specifies gem version of Rails to use when vendor/rails is not present
 
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
-ENV['IDLIKA_VERSION'] = '0.1.50'
+ENV['IDLIKA_VERSION'] = '0.1.51'
 ENV['DEFAULT_REGISTRY_NAME'] = 'Recently Added'
 
 ENV['S3_BUCKET'] = "idlika.com"
